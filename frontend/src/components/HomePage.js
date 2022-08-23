d14ecb49424d5583e722fcae9a9275735d6a322e
Vladimir Budjen <bvalda88@gmail.com>
2022-08-24 00:44:18 +0200

    Small styling

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,6 +1,6 @@
 import {Col, Container, Row, Table, Card} from "react-bootstrap";
 import "../css/Table.css"
-import PaginationBar from "./PaginationBar";
+import DataTableFooter from "./DataTableFooter";
 import {useState} from "react";
 import DataTableHeader from "./DataTableHeader";
 import DataTableBody from "./DataTableBody";
@@ -9,14 +9,14 @@ function HomePage() {
 
     const cat = {image: "https://i.natgeofe.com/n/46b07b5e-1264-42e1-ae4b-8a021226e2d0/domestic-cat_thumb.jpg", name: "Miluje", price:"333", city:"NS", category:"Tools"}
 
-    let items =  new Array(23).fill(cat);
+    let items =  new Array(43).fill(cat);
     const categories = ['All', 'Clothing', 'Tools', 'Sports', 'Accessories', 'Furniture', 'Pets', 'Games', 'Books', 'Technology']
     let userLoggedIn = false;
     const [currentPage, setCurrentPage] = useState(1);
     const [nameFilterValue, nameFilterValueChange] = useState('');
     const [mineOnly, mineOnlyChange] = useState(false);
-    const [price, priceChange] = useState(categories[0]);
-    const [category, categoryChange] = useState();
+    const [price, priceChange] = useState("min");
+    const [category, categoryChange] = useState(categories[0]);
 
 
     return (
@@ -39,8 +39,8 @@ function HomePage() {
                                 <DataTableBody userLoggedIn={userLoggedIn} data={items} currentPage={currentPage}/>
                             </Card.Body>
                             <Card.Footer  className="text-muted d-flex justify-content-center table-footer">
-                                <PaginationBar numberOfPages={Math.ceil(items.length/20)} currentPage={currentPage}
-                                               setCurrentPage={setCurrentPage}
+                                <DataTableFooter numberOfPages={Math.ceil(items.length/20)} currentPage={currentPage}
+                                                 setCurrentPage={setCurrentPage}
                                 />
                             </Card.Footer>
                         </Card>
